141e1ea2e2f2fe1795a3b542d26e91c1f458f146
AndrewPochapsky <andrew.pochapsky@gmail.com>
2017-11-04 09:28:35 -0400

    Basic classes created

---
+++ Assets/Scripts/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comment  {
+    public string Uploader { get; private set; }
+    public string Message { get; private set; }
+
+    public Comment(string uploader, string message)
+    {
+        Uploader = uploader;
+        Message = message;
+    }
+    
+}
---
+++ Assets/Scripts/Memory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory : MonoBehaviour {
+
+    public string Uploader { get; set; }
+    public string Message { get; set; }
+    public List<Comment> Comments { get; set; }
+    public int NumberOfLikes { get; protected set; }
+
+    public void IncrementLike()
+    {
+        NumberOfLikes++;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    
+}
