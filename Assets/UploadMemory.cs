4dc5f5ae0b1d681da6672833fa4f82f71c7fbf6e
[Erica-Wang] <[erica.wang.moyang@gmail.com]>
2017-11-04 09:41:39 -0400

    Upload file

---
+++ Assets/UploadMemory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UploadMemory : MonoBehaviour {
+
+	public TMP_InputField input;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Upload () {
+		String s = input.text;
+
+	}
+}
